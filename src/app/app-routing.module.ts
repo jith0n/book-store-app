4c959694cf2288384ddc545785e10472e65afcb6
jithon <58655915+jithon@users.noreply.github.com>
2021-10-22 16:10:31 +0530

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/components/login/login.component';
+import { ResetPwComponent } from './auth/components/reset-pw/reset-pw.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { BookDetailsComponent } from './books/components/book-details/book-details.component';
+import { ListBooksComponent } from './books/components/list-books/list-books.component';
+import { HomeComponent } from './home/components/home/home.component';
+import { CartComponent } from './shopping/components/cart/cart.component';
+import { CheckoutComponent } from './shopping/components/checkout/checkout.component';
+import { ConfirmationComponent } from './shopping/components/confirmation/confirmation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'books', component:ListBooksComponent},
+  {path:'book-details', component:BookDetailsComponent},
+  {path:'cart',component:CartComponent},
+  {path:'checkout',component:CheckoutComponent},
+  {path:'confirm',component:ConfirmationComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent},
+  {path:'reset-password',component:ResetPwComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,36 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/components/home/home.component';
+import { ListBooksComponent } from './books/components/list-books/list-books.component';
+import { BookDetailsComponent } from './books/components/book-details/book-details.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { SignupComponent } from './auth/components/signup/signup.component';
+import { ResetPwComponent } from './auth/components/reset-pw/reset-pw.component';
+import { CartComponent } from './shopping/components/cart/cart.component';
+import { CheckoutComponent } from './shopping/components/checkout/checkout.component';
+import { ConfirmationComponent } from './shopping/components/confirmation/confirmation.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { MenuComponent } from './shared/components/menu/menu.component';
+import { BookCardComponent } from './books/components/book-card/book-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ListBooksComponent,
+    BookDetailsComponent,
+    LoginComponent,
+    SignupComponent,
+    ResetPwComponent,
+    CartComponent,
+    CheckoutComponent,
+    ConfirmationComponent,
+    HeaderComponent,
+    FooterComponent,
+    MenuComponent,
+    BookCardComponent
   ],
   imports: [
     BrowserModule,
